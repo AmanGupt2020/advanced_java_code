08435535f5e075004c4c14bee2253953ab7af925
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-10 15:55:22 +0530

    Add files via upload

---
+++ Chapter_3_practice_set.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Chapter_3_practice_set {
+    public static void main(String[] args){
+        //-------------QUESTIN 1--------------------------
+     //    String name="Aman Gupta";
+    //    String s= name.toLowerCase();
+    //     System.out.println(s);
+
+        //----------------question 2------------------
+     //   String s= "Aman  Gupta";
+      //   s= s.replace(' ','_');
+     //  System.out.println(s);
+
+        //--------------QUESTION 3-----------------------
+       /*    String letter;
+           letter="Dear <|name|>, thanks a lot!";
+                   letter= letter.replace("<|name|>","aman");
+        System.out.println(letter); */
+
+        //-------------------QUESTION 4---------------------
+      /*  String myString= "my name  is aman   gupta";
+        int my1String= myString.indexOf("  ");
+        int S= myString.indexOf("   ");
+        System.out.println(my1String);
+        System.out.println(S); */
+
+        //-----------------QUESTION 5------------------------
+        String letter= "Dear Harry,\n\t This java course is nice.\n\t Thanks!";
+        System.out.println(letter);
+
+    }
+}
