1b558c63eaa43755ab143ec01274901e879d67ce
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-10 15:55:33 +0530

    Add files via upload

---
+++ Chapter_4_conditionals.java
@@ -0,0 +1,54 @@
+package com.company;
+import java.util.Scanner;
+public class Chapter_4_conditionals {
+    public static void main(String[] args) {
+   //     int age ;
+   //     Scanner s= new Scanner(System.in);
+   //     age= s.nextInt();
+   //     if(age>18){
+   //         System.out.println("yes! you can drive");
+   //     }
+   //     else {
+   //         System.out.println("no! can't drive yet");
+   //     }
+
+//---------------------------------------------------------------------------------------------------------
+     //   Scanner s= new Scanner(System.in);
+      //  int a = s.nextInt();
+        //--------OR------
+     //   boolean b= (a>=0);
+     //   if(b){
+        //________________
+     //    if(a>=0){
+     //        System.out.println(" a is a positive number");
+    //    }
+   //     else{
+  //          System.out.println("a is a negative number");
+        //  }
+
+       //------------------LOGICAL OPERATOR-----------------------
+    //    boolean b1= true;
+    //    boolean b2= false;
+    //    if(b1&&b2){
+        //--------or---
+        //if(b1||b2)
+        //-------------
+    //        System.out.println("y");
+    //    }
+    //    else{
+   //         System.out.println("n");
+   //     }
+
+  //---------------not ----------
+  //      boolean a= true;
+  //      boolean b= false;
+  //      System.out.println("not(a) is -");
+  //      System.out.println(!a);
+  //      System.out.println("not(b) is -");
+  //      System.out.println(!b)//;
+
+
+
+
+     }
+}
