fae6e8cdf906176567d5f31fb8fe17977e3ea86a
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-13 22:03:46 +0530

    Add files via upload

---
+++ Exp1_1.java
@@ -0,0 +1,11 @@
+//Using Scanner Class
+package com.company;
+import java.util.Scanner;
+public class Exp1_1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s;
+        s=sc.nextLine();
+        System.out.println(s);
+    }
+}
