1c141e6f64233c69bc17b9b88e765b8c8e72861b
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-13 22:03:13 +0530

    Add files via upload

---
+++ exception_2.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class exception_2 extends exception_1{
+    public static void main(String[] args){
+        exception_2 o=new exception_2();
+        System.out.println(o.a/o.b);
+    }
+}
