6e97fb5794636cb128ae74d256433ffc9ad1408b
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-14 20:55:28 +0530

    Add files via upload

---
+++ importFromClass.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class importFromClass {
+    public static void main(String[] args){
+        student ob=new student("AMAN GUPTA","20BCE10347");
+        System.out.println(ob.add());
+
+    }
+}
