7792dabfc3dca5f8774410435f15a7ce27db446d
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-14 20:58:11 +0530

    Add files via upload

---
+++ JavaBeans1.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class JavaBeans1 {
+    public static void main(String args[]){
+        JavaBeans e=new JavaBeans();
+        e.setId(10347);
+        e.setName("AMAN GUPTA");
+        System.out.println(e.getId());
+        System.out.println(e.getName());
+    }
+
+}
