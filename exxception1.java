190d0586d5995a0946a3444ed217e25af4a85163
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-14 20:54:52 +0530

    Add files via upload

---
+++ exxception1.java
@@ -0,0 +1,30 @@
+package com.company;
+
+ /*public class exxception1 {
+    public static void main(String[] args){
+        try{
+            int b=1/0;
+
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+            System.out.println("error");
+        }
+        System.out.println("executed");
+    }
+} */
+
+/* public class exxception1{
+    public static void main(String[] args){
+        try{
+            int b=25/5;
+            System.out.println(b);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e);
+            System.out.println("error");
+        }
+        System.out.println("executed");
+    }
+} */
+
