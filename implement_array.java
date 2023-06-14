29a978c919bddcc6507460c6dbf48cfa458442f3
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-14 20:55:09 +0530

    Add files via upload

---
+++ implement_array.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class implement_array {
+    public static void main(String[] args){
+        int[] x={1,2,3,4,5};
+        int length= x.length;;
+        for (int i=0;i<length;i++)
+        {
+            System.out.println(x[i]);
+        }
+    }
+}
