fd1a3db61cd813e5249e134e7efa1dfa8cc2bdce
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-16 23:35:29 +0530

    Add files via upload

---
+++ voting_condition.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class voting_condition {
+    public static void main(String[] args){
+        int x=18;
+            if (x > 18) {
+                System.out.println("you are eligible for voting");
+            }
+
+       else{
+            System.out.println("you are not eligible for voting");
+        }
+    }
+}
